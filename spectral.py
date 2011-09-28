e1ce21d247c45fc2c5822c9c1d76aec5e695b1b8
craig <ccor5588@gmail.com>
2011-09-28 04:11:17 -0500

    Added diffusion wavelets to spectral.py. modified policy iteration code in rl.py - needs cleanup. Overcomplete / linearly dependent basis causes singular matrix error in lstd; what to do?

--- spectral.py
+++ spectral.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+from sparseQR import sparseQR
 
 def laplacian_operator(W):
     D = np.diag(np.sum(W,1))
@@ -13,7 +14,42 @@ def diffusion_operator(W):
     D = np.diag(np.sum(W,1))
     D_invsqrt = np.sqrt(np.linalg.inv(D))
 
-    return np.dot(np.dot(D_invsqrt,W),D_invsqrt) # normalized operator, or...
+    return np.dot(np.dot(D_invsqrt,W),D_invsqrt)
+
+def dw_tree(T,J,lam,p,eps_scal):
+    Q_dict = {}
+    print 'building diffusion wavelet tree'
+    for j in range(J):
+        print 'level j = ', j
+        eps = np.sum(np.sum(T))/len(T[T>0])*eps_scal # heuristic for setting epsilon, from Bo Liu paper
+        print 'epsilon: ', eps
+        Q = sparseQR(T,eps,lam,p)
+        print 'size of new basis: ',Q.shape
+        Q_dict[j] = Q
+        T = np.dot(np.dot(np.dot(Q.T,T),T),Q) # T^2 in k-space (kxk)
+
+    return Q_dict
+
+def expand_wavelets(Q_dict, k, n):
+    J = len(Q_dict)
+    Q_coarse = np.eye(n)
+    for j in xrange(J):
+        Q_coarse = np.dot(Q_coarse,Q_dict[j])
+        if j == 0: 
+            phi = Q_coarse
+        # put the new (more coarse) column vectors on the left
+        else:
+            phi = np.hstack((Q_coarse,phi))
+
+    # add the constant vector and return 
+    return np.hstack((np.ones((n,1))/float(n),phi[:,:k-1]))
+
+def build_diffusion_basis(T,k,J=8,lam=2.5,p=1,eps_scal=10**-3):
+    n = T.shape[0] # number of states in complete space
+    Q_dict = dw_tree(T,J,lam,p,eps_scal)
+
+    return expand_wavelets(Q_dict,k,n)
+    
     
 def room_adjacency(n = 20,self_loops = True):
     adjacents = np.array([[-1,0],[1,0],[0,-1],[0,1]])
@@ -54,7 +90,8 @@ def plot_functions_on_room(n,F):
     [X,Y] = np.meshgrid(range(n),range(n))
     
     fig = plt.figure()
-    rows = 2
+    if F.shape[1] == 1: rows = 1
+    else: rows = 2
     cols = int(np.ceil(F.shape[1]/float(rows)))
     for i in range(F.shape[1]):
         ax = fig.add_subplot(rows,cols, i+1, projection='3d')
