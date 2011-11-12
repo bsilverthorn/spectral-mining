8cb29374ac2c8b3d4d311479e006ed9e179f8f67
Bryan Silverthorn <bcs@cargo-cult.org>
2011-11-12 17:06:16 -0600

    Write actual state tuples in write_ttt_states.

--- specmine/test/test_tictac.py
+++ specmine/test/test_tictac.py
@@ -1,3 +1,7 @@
+import numpy
+import nose.tools
+import specmine
+
 def test_board_state():
     configurations = [
         [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
@@ -6,7 +10,7 @@ def test_board_state():
         [[0, 0, 1], [-1, -1, -1], [0, 1, 0]],
         [[1, 1, -1], [-1, 1, -1], [1, -1, 1]],
         ]
-    boards = [BoardState(numpy.array(c)) for c in configurations]
+    boards = [specmine.tictac.BoardState(numpy.array(c)) for c in configurations]
 
     assert boards[0].get_winner() is None
     assert boards[0].make_move(1, 2, 2)._grid[2, 2] == 1
@@ -17,4 +21,14 @@ def test_board_state():
     assert boards[2].get_winner() == -1
     assert boards[3].get_winner() == -1
     assert boards[4].get_winner() == 0
+
+def test_construct_adjacency_dict():
+    start = specmine.tictac.BoardState()
+    adict = specmine.tictac.construct_adjacency_dict(cutoff = 2)
+    all_first = set((start.make_move(1, i, j), -1) for i in xrange(3) for j in xrange(3))
+
+    nose.tools.assert_equal(adict[(start, 1)], all_first)
+
+    for first in all_first:
+        nose.tools.assert_equal(len(adict[first]), 8)
 
--- specmine/tictac.py
+++ specmine/tictac.py
@@ -1,17 +1,6 @@
-import os.path
-import gzip
-import csv
-import cPickle as pickle
-import contextlib
 import numpy
-import scipy.sparse
-import scipy.sparse.linalg
-import spectral
 
 class BoardState(object):
-    # XXX use a simple bitstring board representation?
-    # XXX an efficient end-state check could be made against a mask set
-
     def __init__(self, grid = None):
         if grid is None:
             grid = numpy.zeros((3, 3))
@@ -94,24 +83,38 @@ class BoardState(object):
         return self._grid
 
 def construct_adjacency_dict(init_board = None, cutoff = None):
-    states = {}
+    """Build a map from (board, player) states to neighboring [state] lists."""
+
+    adict = {}
+
+    def board_recurse(player, parent, board, depth = 0):
+        if cutoff is not None and depth == cutoff:
+            return
+
+        if board.check_end():
+            return
+
+        state = (board, player)
+
+        for i in xrange(3):
+            for j in xrange(3):
+                if board._grid[i,j] == 0:
+                    next_player = -1 * player
+                    next_board = board.make_move(player, i, j)
+
+                    adjacent = adict.get(state)
+
+                    if adjacent is None:
+                        adjacent = adict[state] = set()
+
+                    adjacent.add((next_board, next_player))
+
+                    board_recurse(next_player, board, next_board, depth + 1)
 
     if init_board is None:
         init_board = BoardState()
 
-    def board_recurs(player, parent, board, depth = 0):
-        print len(states)
+    board_recurse(1, None, init_board)
 
-        if cutoff is not None and depth == cutoff:
-            return
+    return adict
 
-        adjacent = states.get(board)
-
-    for i in xrange(3):
-        for j in xrange(3):
-            if board._grid[i,j] == 0:
-                board_recurs(-1 * player, board, board.make_move(player, i, j), depth + 1)
-
-    board_recurs(1, None, init_board)
-
-    return states
