29c526417f30fc156426edffb3b89e3c36c0dd39
Craig Corcoran <ccor@cs.utexas.edu>
2011-12-05 15:06:05 -0600

    debugging go_graph

--- specmine/experiments/go_graph.py
+++ specmine/experiments/go_graph.py
@@ -1,25 +1,35 @@
+import cPickle as pickle
 import numpy
-import sklearn.neighbors
 import specmine
-import go
+import specmine.go
 
 def main(num_games=10, num_samples=1e5):
     
-    game_dict = specmine.util.openz(specmine.util.satic_path('go_games/2010-01.pickle.gz'))
-    
+    with specmine.util.openz(specmine.util.static_path\
+      ('go_games/2010-01.pickle.gz')) as games_file:
+
+        game_dict = pickle.load(games_file)
+        print 'num_games: ', len(game_dict.values())
     # build the affinity representation
 
-    affinity_vectors = numpy.zeros((0,9,9))
-    for game in game_dict.values():
-        numpy.vstack(affinity_vectors,game.grids)
+    all_grids = [game.grids for game in game_dict.itervalues()]
+    affinity_vectors = numpy.vstack(all_grids)
 
     num_boards = affinity_vectors.shape[0]
     print 'number of boards: ', num_boards
 
     affinity_vectors = numpy.reshape(affinity_vectors,(num_boards,81))
+    print 'affinity vector shape: ', affinity_vectors.shape
+    print 'total num boards: ', num_boards
+    print 'subsampling states'
     affinity_vectors = affinity_vectors[numpy.random.permutation(num_boards),:]
     affinity_vectors = affinity_vectors[:num_samples]
 
-    graph = specmine.discovery.affinity_graph(affinity_vectors)
+    print 'building affinity graph'
+    graph = specmine.discovery.affinity_graph(affinity_vectors,neighbors=5)
+    print graph[0]
+    print graph.shape
     # what to do with this graph?
 
+if __name__ == "__main__":
+    main()
