90959bc437231fe3c54352fd2da86a4c2007c137
Bryan Silverthorn <bcs@cargo-cult.org>
2011-12-07 00:21:24 -0600

    Default to the arpack eigensolver.

--- specmine/spectral.py
+++ specmine/spectral.py
@@ -78,7 +78,7 @@ def expand_wavelets(phi_dict, psi_dict, k, n):
     # add the constant vector and return 
     return np.hstack((np.ones((n,1))/float(n),basis[:,:k-1]))
 
-def laplacian_basis(W, k, largest = False, method = "amg"):
+def laplacian_basis(W, k, largest = False, method = "arpack"):
     """Build laplacian basis matrix with k bases from weighted adjacency matrix W."""
 
     logger.info(
