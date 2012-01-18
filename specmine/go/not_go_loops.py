aed366b358e90e7fda5a8da69c2b9a0cb0567d37
craig <ccor5588@gmail.com>
2012-01-18 08:44:02 -0600

    added not_go_loops because I keep getting a seg fault when using cythonic go_loops

---
+++ specmine/go/not_go_loops.py
@@ -0,0 +1,33 @@
+import numpy
+
+def applyTemplates(applications, templates, state, num_feats):
+
+    TR = templates.shape[1]
+    TC = templates.shape[2]
+    counts = numpy.zeros(num_feats, numpy.uint32)
+
+    for i in xrange(applications.shape[0]):
+        r = applications[i,0]
+        c = applications[i,1]
+        t = applications[i,2]
+        f = applications[i,3]
+
+        match = True
+
+        for tr in xrange(TR):
+            for tc in xrange(TC):
+                if templates[t, tr, tc] != state[r + tr, c + tc]:
+                    match = False
+
+                    break
+
+            if not match:
+                break
+
+        if match:
+            counts[f] += 1
+
+    ind = counts.nonzero()[0] #  weight indices where the number of features present was nonzero
+    counts = counts[ind]
+
+    return (ind, counts)
