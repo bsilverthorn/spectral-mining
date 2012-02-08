dc82acde7622ab278eafc2821927e63ea81ef637
Craig Corcoran <ccor@cs.utexas.edu>
2012-02-08 13:08:00 -0600

    testing crossval

--- specmine/science.py
+++ specmine/science.py
@@ -66,6 +66,8 @@ def score_features_predict(feature_map, values, folds = 10, alpha = 1.0):
     # run the experiment
     ridge = sklearn.linear_model.Ridge(alpha = alpha, normalize = True)
 
+    from ipdb import set_trace; set_trace()
+
     k_fold_cv = sklearn.cross_validation.KFold(len(states), folds)
     scores = \
         sklearn.cross_validation.cross_val_score(
