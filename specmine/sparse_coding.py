e86fa7f80c7479a5811603199312b4ff689a866b
Craig Corcoran <ccor@cs.utexas.edu>
2012-03-06 10:26:32 -0600

    edits to sparse coding

--- specmine/sparse_coding.py
+++ specmine/sparse_coding.py
@@ -1,8 +1,8 @@
 import pickle
 import numpy
 import specmine
-#import pyplot.matplotlib as pl
-import pylab as pl
+import pyplot.matplotlib as pl
+#import pylab as pl
 from time import time
 from sklearn.decomposition import MiniBatchDictionaryLearning
 
@@ -23,6 +23,10 @@ def main(games_path = None):
 
     print 'boards shape: ', boards.shape
 
+    boards = boards.reshape((boards.shape[0],-1))
+
+    print 'boards reshaped: ', boards.shape
+
     print 'Learning the dictionary... '
     t0 = time()
     dico = MiniBatchDictionaryLearning(n_atoms=100, alpha=1, n_iter=500)
@@ -30,17 +34,16 @@ def main(games_path = None):
     dt = time() - t0
     print 'done in %.2fs.' % dt
 
-    pl.figure(figsize=(4.2, 4))
+    #pl.figure(figsize=(4.2, 4))
     for i, comp in enumerate(V[:100]):
         pl.subplot(10, 10, i + 1)
-        pl.imshow(comp.reshape(patch_size), cmap=pl.cm.gray_r,
-              interpolation='nearest')
+        pl.imshow(comp, cmap=pl.cm.gray_r) # interpolation='nearest')
         pl.xticks(())
         pl.yticks(())
-        pl.suptitle('Dictionary learned from Lena patches\n' +
-            'Train time %.1fs on %d patches' % (dt, len(data)),
-            fontsize=16)
-        pl.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
+        #pl.suptitle('Dictionary learned from Lena patches\n' +
+            #'Train time %.1fs on %d patches' % (dt, len(data)),
+            #fontsize=16)
+        #pl.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
 
 if __name__ == '__main__':
     main()
